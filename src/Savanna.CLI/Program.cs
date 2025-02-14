38aa1e18bc811ac0e960f8c9986cf4bfbca3636b
aivaras.armalis <aivaras.net@gmail.com>
2025-02-14 11:54:21 +0200

    Initial commit

---
+++ src/Savanna.CLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Savanna.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
